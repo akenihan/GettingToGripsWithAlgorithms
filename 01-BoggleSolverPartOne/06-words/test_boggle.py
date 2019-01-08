11d0f50f76006642d0e7bad457b3337b95ee033b
akenihan <46343185+akenihan@users.noreply.github.com>
2019-01-08 01:23:02 +0000

    Lesson code

--- 01-BoggleSolverPartOne/06-words/test_boggle.py
+++ 01-BoggleSolverPartOne/06-words/test_boggle.py
@@ -4,22 +4,12 @@ from string import ascii_uppercase
 
 
 class TestBoggle(unittest.TestCase):
-    """
-    Our test suite for boggle solver
-    """
 
     def test_can_create_an_empty_grid(self):
-        """
-        Test to see if we can create an empty grid
-        """
         grid = boggle.make_grid(0, 0)
         self.assertEqual(len(grid), 0)
     
     def test_grid_size_width_times_height(self):
-        """
-        Test is to ensure that the total size of the grid
-        is equal to width * height
-        """
         grid = boggle.make_grid(2, 3)
         self.assertEqual(len(grid), 6)
     
@@ -32,18 +22,11 @@ class TestBoggle(unittest.TestCase):
         self.assertNotIn((2, 2), grid)
     
     def test_grid_is_filled_with_letters(self):
-        """
-        Ensure that each of the coordinates in the grid
-        contains letters
-        """
         grid = boggle.make_grid(2, 3)
         for letter in grid.values():
             self.assertIn(letter, ascii_uppercase)
     
     def test_neighbours_of_a_position(self):
-        """
-        Ensure that a position has 8 neighbours
-        """
         coords = (1, 2)
         neighbours = boggle.neighbours_of_position(coords)
         self.assertIn((0, 1), neighbours)
@@ -57,9 +40,6 @@ class TestBoggle(unittest.TestCase):
     
 
     def test_all_grid_neighbours(self):
-        """
-        Ensure that all of the grid positions have neighbours
-        """
         grid = boggle.make_grid(2, 2)
         neighbours = boggle.all_grid_neighbours(grid)
         self.assertEqual(len(neighbours), len(grid))
@@ -69,9 +49,6 @@ class TestBoggle(unittest.TestCase):
             self.assertListEqual(sorted(neighbours[pos]), sorted(others))
     
     def test_converting_a_path_to_a_word(self):
-        """
-        Ensure that paths can be converted to words
-        """
         grid = boggle.make_grid(2, 2)
         oneLetterWord = boggle.path_to_word(grid, [(0, 0)])
         twoLetterWord = boggle.path_to_word(grid, [(0, 0), (1, 1)])
@@ -79,9 +56,6 @@ class TestBoggle(unittest.TestCase):
         self.assertEqual(twoLetterWord, grid[(0, 0)] + grid[(1, 1)])
     
     def test_search_grid_for_words(self):
-        """
-        Ensure that certain patterns can be found in a `path_to_word`
-        """
         grid = {(0, 0): 'A', (0, 1): 'B', (1, 0): 'C', (1, 1): 'D'}
         twoLetterWord = 'AB'
         threeLetterWord = 'ABC'
@@ -95,10 +69,6 @@ class TestBoggle(unittest.TestCase):
         self.assertTrue(notThereWord not in foundWords)
     
     def test_load_dictionary(self):
-        """
-        Test that the `get_dictionary` function returns a dictionary
-        that has a length greater than 0
-        """
         dictionary = boggle.get_dictionary('words.txt')
         self.assertGreater(len(dictionary), 0)
     
