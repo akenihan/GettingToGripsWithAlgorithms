1827eff071c47c93f99344db4dd70ec71db1944c
akenihan <46343185+akenihan@users.noreply.github.com>
2019-01-08 00:51:05 +0000

    Update boggle.py

--- 01-BoggleSolverPartOne/04-grid_content/boggle.py
+++ 01-BoggleSolverPartOne/04-grid_content/boggle.py
@@ -1,1 +1,8 @@
+from string import ascii_uppercase
+from random import choice
 
+
+def make_grid(width, height):
+    return {(row, col): choice(ascii_uppercase)
+        for row in range(height)
+        for col in range(width)}
