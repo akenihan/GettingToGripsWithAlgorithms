f026713656d49c4e587d9853c84f06548bfc36eb
akenihan <46343185+akenihan@users.noreply.github.com>
2019-01-08 00:45:59 +0000

    Update boggle.py

--- 01-BoggleSolverPartOne/03-defining_the_grid/boggle.py
+++ 01-BoggleSolverPartOne/03-defining_the_grid/boggle.py
@@ -1,7 +1,1 @@
-def make_grid(width, height):
-    """
-    Creates a grid that will hold all of the tiles
-    for a boggle game
-    """
-    return {(row, col): ' ' for row in range(height)
-        for col in range(width)}
+
